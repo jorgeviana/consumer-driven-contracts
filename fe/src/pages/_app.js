0eb194da142784aaaef550b736db2d5ba289b49e
Jorge Viana <jorge.viana@gft.com>
2025-07-02 11:25:25 +0200

    fe - remove commented code

--- fe/src/pages/_app.js
+++ fe/src/pages/_app.js
@@ -3,11 +3,7 @@
 import { UserProvider, useUser } from '../context/UserContext';
 
 import Login from './Login';
-
-import EquityChart from '../app/EquityChart';
 import EquitySelector from '../app/EquitySelector';
-import SentimentPanel from '../app/SentimentPanel';
-import Logout from './Logout';
 
 
 function InnerApp({ Component, pageProps }) {
@@ -22,10 +18,6 @@ function InnerApp({ Component, pageProps }) {
       <div>
         <EquitySelector />
       </div>
-      {/* <div className="space-y-6">
-        <SentimentPanel />
-      </div>
-      <div><Logout /></div> */}
     </>
   );
 }
